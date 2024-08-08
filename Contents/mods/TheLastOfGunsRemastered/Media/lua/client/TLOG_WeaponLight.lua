f38cfc61785c159aee2cb355ed7d0a69b5ae2a55
SirDoggyJvla <106160594+SirDoggyJvla@users.noreply.github.com>
2024-08-08 21:22:11 +0200

    Rework of WeaponLight
    - module to access functions
    - optimizations
    - ability to check for multiple attachments
    - remove VFE compatibility shit with our own tag

--- Contents/mods/TheLastOfGunsRemastered/Media/lua/client/TLOG_WeaponLight.lua
+++ Contents/mods/TheLastOfGunsRemastered/Media/lua/client/TLOG_WeaponLight.lua
@@ -1,64 +1,97 @@
-local FlashlightTag = function(character, inventoryItem, player)
-    	if inventoryItem ~= nil then
-            if inventoryItem:getStringItemType() == "RangedWeapon" then
-				if inventoryItem:getSling() then
-					local Sling = inventoryItem:getSling():getFullType()
-                        if Sling  == "Base.TLOG_WeaponFlashlight" then
-							if (inventoryItem:hasTag("WeaponLight")) then return end
-							inventoryItem:getTags():add("WeaponLight")
-                            print("If sling is the flashlight, should add tag")
-							end
-							if Sling  ~= "Base.TLOG_WeaponFlashlight" then
-							inventoryItem:getTags():remove("WeaponLight")
-							print("Diffrent Sling, removes the tag")
-							--inventoryItem:setLightStrength(0.0)
-						end
-				else
-                            inventoryItem:getTags():remove("WeaponLight")
-							print("No Sling at all, removes the tag")
-							--inventoryItem:setLightStrength(0.0)
+local TLOG_WeaponLight = {}
 
-                end
-			end
-        end
+TLOG_WeaponLight.Flashlights = {
+	"Base.TLOG_WeaponFlashlight",
+
+}
+
+-- Check gun has the proper flashlight attachment
+-- add the tag to the weapon if it has a flashlight or remove it
+---@param attachment_slot string
+---@return boolean
+TLOG_WeaponLight.CheckForFlashlight = function(attachment_slot)
+	for _,v in ipairs(TLOG_WeaponLight.Flashlights) do
+		if attachment_slot == v then return true end
 	end
 
+	return false
+end
 
--- Code from VFE, won't run if VFE is active
-local function TLOGWeaponLightBeam()
+-- add the tag to the weapon if it has a flashlight or remove it
+---@param character IsoPlayer
+---@param inventoryItem InventoryItem
+TLOG_WeaponLight.FlashlightTag = function(character, inventoryItem)
+	-- skip if not ranged weapon
+	if not inventoryItem or inventoryItem:getStringItemType() ~= "RangedWeapon" then return end
+	---@cast inventoryItem HandWeapon
 
-	local attacker	= getSpecificPlayer(0)
-	local weapon	= nil
+	-- get tag
+	local sling = inventoryItem:getSling()
+	local tag = inventoryItem:hasTag("TLOG_WeaponLight")
 
-	-- A dirty solution that turns off flashlights of weapons in inventory every update
-	local playerItems = attacker:getInventory():getItems()
-	for i=1,playerItems:size() do
-		weapon = playerItems:get(i-1)
-		if (weapon:hasTag("WeaponLight")) then
-			weapon:setLightStrength(0.0)
-			break
+	-- if sling
+	if sling then
+		-- verify sling type is weapon flashlight
+		local slingCheck = TLOG_WeaponLight.CheckForFlashlight(sling:getFullType())
+
+		-- if not tag
+		if not tag then
+
+			-- and has flashlight
+			if slingCheck then
+				inventoryItem:getTags():add("TLOG_WeaponLight")
+			end
+
+		-- if not flashlight and has tag, then remove tag
+		elseif not slingCheck then
+			inventoryItem:getTags():remove("TLOG_WeaponLight")
+		end
+
+	-- no sling, delete tag if has tag
+	elseif tag then
+		inventoryItem:getTags():remove("TLOG_WeaponLight")
+	end
+end
+
+
+-- Code from VFE, fixed a few things. It will stop lights on weapons
+---@param player IsoPlayer
+TLOG_WeaponLight.WeaponLightBeam = function(player)
+	local player_weapon = player:getPrimaryHandItem()
+
+	-- get weapon
+	if player_weapon then
+		-- check has light tag
+		local tag = player_weapon:hasTag("TLOG_WeaponLight")
+		if tag then
+			-- set light on or off based on player aiming
+			if player:isAiming() then
+				player_weapon:setLightStrength(1.5)
+			else
+				player_weapon:setLightStrength(0.0)
+			end
 		end
 	end
-	
-	
-	if attacker ~= nil then				
-		weapon	= attacker:getPrimaryHandItem()
-	end
-	if weapon == nil then return end
-	
-	if attacker:isAiming() and attacker:getPrimaryHandItem() and (weapon:hasTag("WeaponLight")) then
-		weapon:setLightStrength(1.5)
-	elseif not attacker:isAiming() and attacker:getPrimaryHandItem() and (weapon:hasTag("WeaponLight")) then
-		weapon:setLightStrength(0.0)
+
+	-- iterates through player items and deactivate the light for weapons if not the one equiped
+	local weapon
+	local inventory = player:getInventory():getItems()
+	for i=0,inventory:size() - 1 do
+		weapon = inventory:get(i)
+		if weapon:hasTag("TLOG_WeaponLight") and (not player_weapon or player_weapon ~= weapon) then
+			weapon:setLightStrength(0.0)
+		end
 	end
 end
 
-if not getActivatedMods():contains("VFExpansion1") then
-	print("Did not found VFE Active")
-Events.OnPlayerUpdate.Add(TLOGWeaponLightBeam)
-end
-Events.OnEquipPrimary.Add(FlashlightTag)
+-- handle flashlight
+Events.OnPlayerUpdate.Add(TLOG_WeaponLight.WeaponLightBeam)
+
+-- add the tag on game launch
+Events.OnEquipPrimary.Add(TLOG_WeaponLight.FlashlightTag)
 Events.OnGameStart.Add(function()
 	local player = getPlayer()
-	FlashlightTag(player, player:getPrimaryHandItem())
+	TLOG_WeaponLight.FlashlightTag(player, player:getPrimaryHandItem())
 end)
+
+return TLOG_WeaponLight
